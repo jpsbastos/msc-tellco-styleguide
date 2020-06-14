a500c2ec635f02ce6a5487caaac4d065ae6b3ce9
João Bastos <ctw00887@criticaltechworks.com>
2020-06-14 16:37:52 +0100

    Minor change

--- src/msc-tellco-styleguide.ts
+++ src/msc-tellco-styleguide.ts
@@ -1,8 +1,14 @@
 import "./set-public-path";
 // import "./styles/global.scss?modules=false";
 // Anything exported from this file is importable by other in-browser modules.
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas, faStar, faCheck, faTimes, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  fas,
+  faStar,
+  faCheck,
+  faTimes,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 library.add(fas, faStar, faCheck, faTimes, far, faShoppingCart);
